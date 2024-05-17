7107174c80e1d02ea2907e59bc80b1856dd9ecf8
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-05-17 15:43:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date? 
+        (!isNaN(req.params.date)? new Date(parseInt(req.params.date)) : new Date(parseInt(req.params.date))) : 
+         new Date();
+  if(isNaN(date)){
+    return res.json({ error : "Invalid Date" });
+  }
+  res.json({utc: date.toUTCString(), unix: Date.parse(date)});
 });
 
 
